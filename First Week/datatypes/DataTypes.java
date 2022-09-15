f9ec19ac98dc06be9d8b618d12bfc9feb0066256
Beytullah Çetin <pasaport_07@hotmail.com>
2022-09-15 23:49:43 +0300

    Ders 10'a kadar

---
+++ First Week/datatypes/DataTypes.java
@@ -0,0 +1,20 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        
+        int sayi = 12;
+        // sayi = "Ankara"; Java tip güvenli bir dil olduğu için int değişkene string ataması yapılamaz.
+
+        byte byteSayi = 127; // byte değere -128 ve 127 arasında atama yapılabilir.
+
+        // byte, short, int, long tam sayıları tutmak için
+        // float ve double ise ondalıklı sayıları tutmak için kulanılır.
+
+        // char tek bir karakter tutmak için kullanılır.
+        char karakter = 'A';
+
+        //boolean true ve false değerlerini tutmak için kullanılır. 0 - 1
+        boolean dogruMu = false;
+
+
+    }
+}
